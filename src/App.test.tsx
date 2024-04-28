4a5d0bbbe5fb6b6fc151c0f26cc984d21c2a2cc2
Eurydice Soderquist <contact@dakkra.com>
2024-04-28 11:23:15 -0600

    First working test :D

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,39 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import React, {act} from 'react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import App from './App';
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const queryClient = new QueryClient();
+
+test('Login button enabled and disables correctly', () => {
+    act(() => {
+        render(<QueryClientProvider client={queryClient}><App/></QueryClientProvider>);
+    })
+
+    const usernameInput = screen.getByPlaceholderText(/username/i)
+    expect(usernameInput).toBeInTheDocument();
+    const passwordInput = screen.getByPlaceholderText(/password/i)
+    expect(passwordInput).toBeInTheDocument();
+
+    const loginButton = screen.getByRole("button", {name: /login/i});
+    expect(loginButton).toBeInTheDocument();
+    expect(loginButton).toBeDisabled();
+
+    act(() => {
+        usernameInput.focus();
+        fireEvent.change(usernameInput, {target: {value: "test"}});
+
+        passwordInput.focus();
+        fireEvent.change(passwordInput, {target: {value: "test"}});
+    });
+    expect(loginButton).toBeEnabled();
+
+    act(() => {
+        usernameInput.focus();
+        fireEvent.change(usernameInput, {target: {value: ""}});
+
+        passwordInput.focus();
+        fireEvent.change(passwordInput, {target: {value: ""}});
+    });
+    expect(loginButton).toBeDisabled();
 });
